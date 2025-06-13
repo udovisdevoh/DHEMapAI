12fdcf5c12917d55c12e1a2c0de1539c42becdb5
Guillaume Lacasse <glacasse@gmail.com>
2025-06-13 12:01:52 -0400

    Ok, ça semble marcher maintenant, cependant, il y a un bug, les pieces ne sont pas fermées, il y a des diagonales et les pieces ne sont pas connectées entre elles

--- DGraphToDHEMap/MapGenerator.cs
+++ DGraphToDHEMap/MapGenerator.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
 using DGraphBuilder.Models.DGraph;
 using DGraphBuilder.Models.Dhemap;
 
@@ -20,17 +24,45 @@ namespace DGraphBuilder.Generation
 
         public DhemapFile Generate()
         {
-            // Étape 1 : Calculer la disposition physique des pièces sans les chevaucher.
+            // Étape 1 : Calculer la disposition physique des pièces.
             Console.WriteLine("Étape 1: Calcul de la disposition des pièces...");
             var layoutEngine = new LayoutEngine(_dgraph, _random);
             var physicalLayout = layoutEngine.CalculateLayout();
 
-            // Étape 2 : Construire la géométrie DHEMap concrète à partir de la disposition calculée.
+            // ### CORRECTION : Étape 1.5 - Normaliser les coordonnées ###
+            Console.WriteLine("Étape 1.5: Normalisation des coordonnées de la carte...");
+            NormalizeLayoutPositions(physicalLayout);
+
+            // Étape 2 : Construire la géométrie DHEMap concrète à partir de la disposition normalisée.
             Console.WriteLine("\nÉtape 2: Construction de la géométrie DHEMap...");
             var mapBuilder = new MapBuilder(_dgraph, _random);
             var dhemap = mapBuilder.Build(physicalLayout);
 
             return dhemap;
+        }
+
+        private void NormalizeLayoutPositions(List<PhysicalRoom> layout)
+        {
+            if (!layout.Any()) return;
+
+            float minX = float.MaxValue;
+            float minY = float.MaxValue;
+
+            foreach (var room in layout)
+            {
+                var box = room.GetBoundingBox();
+                if (box.Left < minX) minX = box.Left;
+                if (box.Top < minY) minY = box.Top;
+            }
+
+            // Décalage pour ramener le coin supérieur gauche de la carte près de l'origine (avec une marge).
+            var offsetX = -minX + 128;
+            var offsetY = -minY + 128;
+
+            foreach (var room in layout)
+            {
+                room.Position = new Vector2(room.Position.X + offsetX, room.Position.Y + offsetY);
+            }
         }
     }
 }
