2e1b78cefb5aacba18d1c37beaea20a3c78a7ace
Guillaume Lacasse <glacasse@gmail.com>
2025-06-16 17:52:59 -0400

    ça marche, sauf que le formulaire est trop mince

---
+++ DGenesis/Models/GenesisPromptRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DGenesis.Models
+{
+    public class GenesisPromptRequest
+    {
+        [Required]
+        [Display(Name = "Jeu Cible")]
+        public string Game { get; set; } = "doom2";
+
+        [Display(Name = "Nombre de Pièces")]
+        [Range(3, 100)]
+        public int RoomCount { get; set; } = 12;
+
+        [Display(Name = "Probabilité de Secret (0.0 - 1.0)")]
+        [Range(0.0, 1.0)]
+        public double SecretRoomPercentage { get; set; } = 0.15;
+
+        [Display(Name = "Connectivité Moyenne")]
+        [Range(1.0, 5.0)]
+        public double AvgConnectivity { get; set; } = 2.5;
+
+        [Display(Name = "Delta de Hauteur Moyen")]
+        public int AvgFloorHeightDelta { get; set; } = 48;
+
+        [Display(Name = "Hauteur Moyenne des Pièces")]
+        public int AvgHeadroom { get; set; } = 128;
+
+        [Display(Name = "Envergure Verticale Totale")]
+        public int TotalVerticalSpan { get; set; } = 1024;
+
+        [Display(Name = "Description du Thème Architectural")]
+        [DataType(DataType.MultilineText)]
+        public string ArchitecturalTheme { get; set; }
+
+        [Display(Name = "Description du Gameplay (Monstres, Objets, Ambiance)")]
+        [DataType(DataType.MultilineText)]
+        public string GameplayTheme { get; set; }
+    }
+}
--- DGenesis/Startup.cs
+++ DGenesis/Startup.cs
@@ -1,13 +1,9 @@
+using DGenesis.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DGenesis
 {
@@ -24,6 +20,7 @@ namespace DGenesis
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<GameAssetService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,17 +33,11 @@ namespace DGenesis
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
